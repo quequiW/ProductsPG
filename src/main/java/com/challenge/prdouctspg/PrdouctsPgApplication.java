052ee614c1ab28e74959ee4cc4b354cff0bfea39
quequiW <ezequielpavon1@gmail.com>
2023-05-23 14:16:51 -0300

    Initial commit

---
+++ src/main/java/com/challenge/prdouctspg/PrdouctsPgApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.prdouctspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrdouctsPgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrdouctsPgApplication.class, args);
+    }
+
+}
